e84767fea952df64ec08bceaaaa007b3148484d3
Sean Kennedy <sean@mule.local>
2014-12-22 15:51:14 -0500

    unnested tests and added different commands

--- tests/manual.js
+++ tests/manual.js
@@ -2,57 +2,101 @@
 
 var Command = require('../');
 
-var lsetc = new Command('ls', ['-l'], '/etc');
+var commands = [
+	new Command('ls', ['-l'], '/etc'),
+	new Command('grep', ['sh', '-r', '/usr/bin'], './'),
+	new Command('npm', ['update'], './')
+];
 
-console.log('** default');
-lsetc.run(function () {
-	console.log('** redirect');
-	lsetc.setOptions({
-		redirect: 'redirect.out'
-	});
-	lsetc.run(function () {
-		console.log('** progress true');
-		lsetc.run({progress: true}, function () {
-			console.log('** progress 5');
-			lsetc.run({progress: 5}, function () {
-				console.log('** redirect + progress 1');
-				lsetc.setOptions({
-					redirect: 'redirect2.out',
-					progress: 1
-				});
-				lsetc.run(function () {
-					console.log('** redirect + progress true');
-					lsetc.setOptions({
-						redirect: 'redirect3.out',
-						progress: true
-					});
-					lsetc.run(function () {
-						console.log('** record');
-						lsetc.run({record: 'direct.out'}, function () {
-							console.log('** record + progress 3');
-							lsetc.setOptions({
-								record: 'direct2.out',
-								progress: 3
-							});
-							lsetc.run(function () {
-								console.log('** record + redirect');
-								lsetc.setOptions({
-									record: 'direct3.out',
-									redirect: 'redirect4.out'
-								});
-								lsetc.run(function () {
-									console.log('** record + redirect + progress 8');
-									lsetc.run({
-										record: 'direct4.out',
-										redirect: 'redirect5.out',
-										progress: 8
-									});
-								});
-							});
-						});
-					});
-				});
-			});
+doTests(commands);
+
+function doTests(commands) {
+	if (commands.length === 0) {
+		return;
+	}
+	var command = commands.shift();
+	return testDefault();
+
+	function testDefault() {
+		console.log('** default');
+		command.run(testRedirect);
+	}
+
+	function testRedirect(code) {
+		console.log('return code:', code);
+		console.log('** redirect');
+		command.setOptions({
+			redirect: command.exec + '_redirect.out'
 		});
-	});
-});
+		command.run(testRecord);
+	}
+
+	function testRecord(code) {
+		console.log('return code:', code);
+		console.log('** record');
+		command.run({record: command.exec + '_record.out'}, testProgress);
+	}
+
+	function testProgress(code) {
+		console.log('return code:', code);
+		console.log('** progress true');
+		command.run({progress: true}, function (code) {
+			console.log('return code:', code);
+			console.log('** progress 5');
+			command.run({progress: 5}, testProgressRedirect);
+		});
+	}
+
+	function testProgressRedirect(code) {
+		console.log('return code:', code);
+		console.log('** redirect + progress 1');
+		command.setOptions({
+			redirect: command.exec + '_redirect-progress_1.out',
+			progress: 1
+		});
+		command.run(function (code) {
+			console.log('return code:', code);
+			console.log('** redirect + progress true');
+			command.setOptions({
+				redirect: command.exec + '_redirect-progress_true.out',
+				progress: true
+			});
+			command.run(testRecordRedirect);
+		});
+	}
+
+	function testRecordRedirect(code) {
+		console.log('return code:', code);
+		console.log('** record + redirect');
+		command.setOptions({
+			record: command.exec + '_record-with-redirect.out',
+			redirect: command.exec + '_redirect-with-record.out'
+		});
+		command.run(testRecordProgress);
+	}
+
+	function testRecordProgress(code) {
+		console.log('return code:', code);
+		console.log('** record + progress 3');
+		command.setOptions({
+			record: command.exec + '_record-progress_3.out',
+			progress: 3
+		});
+		command.run(testRecordProgressRedirect);
+	}
+
+	function testRecordProgressRedirect(code) {
+		console.log('return code:', code);
+		console.log('** record + redirect + progress 8');
+		command.run({
+				record: command.exec + '_record-with-redirct-progress_8.out',
+				redirect: command.exec + '_redirect-with-record-progress_8.out',
+				progress: 8
+			},
+			function (code) {
+				console.log('return code:', code);
+				doTests(commands);
+			}
+		);
+	}
+}
